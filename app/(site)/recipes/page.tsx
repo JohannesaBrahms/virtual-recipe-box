3670fc35596ab43ddc33e86ee8e5d35ef102503c
Bojana Armacki Dragutinovic <dragutinovic.bojana@gmail.com>
2024-02-07 14:34:33 +0100

    feat(recipe): get and display all recipes from database [PoC]

--- app/(site)/recipes/page.tsx
+++ app/(site)/recipes/page.tsx
@@ -1,5 +1,29 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useState, useEffect } from 'react';
+
 const RecipesPage = () => {
-  return <div>Recipes Page</div>;
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  useEffect(() => {
+    const getRecipes = async () => {
+      const response = await fetch('/api/recipes');
+      const data = await response.json();
+      setRecipes(data);
+    };
+    getRecipes();
+  }, []);
+  return (
+    <div>
+      <h1>Recipes List</h1>
+      <ul>
+        {recipes.map((item) => (
+          <div key={item.id}>{item.title}</div>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default RecipesPage;
