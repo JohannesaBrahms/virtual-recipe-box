4cd44d62b00c17484fa3085b4a50fd2b66a79142
Bojana Armacki Dragutinovic <dragutinovic.bojana@gmail.com>
2024-02-26 10:40:45 +0100

    style: search field to mui

--- app/components/search.tsx
+++ app/components/search.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { InputAdornment } from '@mui/material';
+import TextField from '@mui/material/TextField';
 import { usePathname, useSearchParams, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
@@ -22,14 +24,21 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   return (
     <div>
-      <input
+      <TextField
+        id="outlined-search"
         placeholder={placeholder}
         defaultValue={searchParams.get('query')?.toString()}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <MagnifyingGlassIcon style={{ width: 18, height: 18 }} />
+            </InputAdornment>
+          ),
+        }}
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
       />
-      <MagnifyingGlassIcon style={{ width: 18, height: 18 }} />
     </div>
   );
 }
