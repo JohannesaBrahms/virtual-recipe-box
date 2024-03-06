827e35c7fdb5ba7b6b93ed6dfb179d6711211e67
Bojana Armacki Dragutinovic <dragutinovic.bojana@gmail.com>
2024-03-06 09:35:59 +0100

    chore(login): updated pass validation

--- schemas/index.ts
+++ schemas/index.ts
@@ -2,7 +2,9 @@ import { z } from 'zod';
 
 export const LoginSchema = z.object({
   email: z.string().email(),
-  password: z.string(),
+  password: z.string().min(1, {
+    message: 'Please enter password',
+  }),
 });
 
 export type LoginSchemaType = z.infer<typeof LoginSchema>;
