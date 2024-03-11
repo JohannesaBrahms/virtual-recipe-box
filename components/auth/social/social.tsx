0b5cf07e61f0e88a3601e5e4b25ada11c62b53f6
Bojana Armacki Dragutinovic <dragutinovic.bojana@gmail.com>
2024-03-11 11:37:34 +0100

    feat(oauth): add google and github as oauth providers

--- components/auth/social/social.tsx
+++ components/auth/social/social.tsx
@@ -1,18 +1,27 @@
 'use client';
 
+import { signIn } from 'next-auth/react';
+
 import { FcGoogle } from 'react-icons/fc';
-import { FaFacebook } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
 import { Button } from '@/components/button';
 import styles from '@/components/auth/social/social.module.css';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 
 export const Social = () => {
+  const onClick = (provider: 'google' | 'github') => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
+
   return (
     <div className={styles.container}>
-      <Button variant="outlined" fullWidth onClick={() => {}}>
+      <Button variant="outlined" fullWidth onClick={() => onClick('google')}>
         <FcGoogle />
       </Button>
-      <Button variant="outlined" fullWidth onClick={() => {}}>
-        <FaFacebook />
+      <Button variant="outlined" fullWidth onClick={() => onClick('github')}>
+        <FaGithub />
       </Button>
     </div>
   );
