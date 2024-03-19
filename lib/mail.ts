b20f04c31cbc6ebca8099cead181eaf478afb3c0
Bojana Armacki Dragutinovic <dragutinovic.bojana@gmail.com>
2024-03-19 09:38:14 +0100

    fea(auth, 2fa): send 2fa email

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,11 +2,13 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+const fromEmail = 'onboarding@resend.dev';
+
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const passworddResetLink = `http://localhost:3000/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: 'onboarding@resend.dev',
+    from: fromEmail,
     to: email,
     subject: 'Reset your password',
     html: `<p>Click <a href="${passworddResetLink}">here</a> to reset your password.</p>`,
@@ -17,9 +19,18 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: 'onboarding@resend.dev',
+    from: fromEmail,
     to: email,
     subject: 'Confirm your email',
     html: `<p>Click <a href="${confirmLink}">here</a> to confim email.</p>`,
+  });
+};
+
+export const sendTwoFactorEmail = async (email: string, token: string) => {
+  await resend.emails.send({
+    from: fromEmail,
+    to: email,
+    subject: '2FA Code',
+    html: `<p>Here is your 2Fa code: ${token}`,
   });
 };
