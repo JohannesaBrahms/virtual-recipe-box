c89610a1752d80f74ea4ff09f03b79996ea3b6a5
Bojana Armacki Dragutinovic <dragutinovic.bojana@gmail.com>
2024-03-21 13:11:59 +0100

    fix(recipe):  create recipe use current user

--- actions/create-recipe.ts
+++ actions/create-recipe.ts
@@ -6,6 +6,8 @@ import { redirect } from 'next/navigation';
 import { db } from '@/lib/db';
 import { CreateRecipeSchema } from '@/lib/types';
 
+import { auth } from '@/auth';
+
 export const createRecipe = async (formData: FormData) => {
   const { title, description, difficulty } = CreateRecipeSchema.parse({
     title: formData.get('title'),
@@ -13,9 +15,18 @@ export const createRecipe = async (formData: FormData) => {
     difficulty: formData.get('difficulty'),
   });
 
+  const session = await auth();
+  const sessionUser = session?.user;
+
+  if (!sessionUser) {
+    return { error: 'Session expired, Please log in to continue.' };
+  }
+
   try {
     await db.$transaction(async (tx) => {
-      const user = await tx.user.findFirst();
+      const user = await tx.user.findUnique({
+        where: { id: sessionUser.id },
+      });
 
       if (user == undefined) {
         throw new Error('Error finding author. Could not create recipe.');
