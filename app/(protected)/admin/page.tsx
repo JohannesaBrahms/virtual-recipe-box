e5966cfe6d3f116661822b97fc33c9efa0c57fe9
Bojana Armacki Dragutinovic <dragutinovic.bojana@gmail.com>
2024-04-03 10:28:08 +0200

    feat(admin): buttons to test admin server action and API route

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -5,14 +5,25 @@ import styles from './page.module.css';
 import { RoleGate } from './_components/role-gate';
 import { FormStatus } from '@/components/form-status/form-status';
 import { UserRole } from '@prisma/client';
+import { Button } from '@/components/button';
 
 const AdminPage = () => {
   return (
     <div className={styles.container}>
       <CardWrapper headerLabel="Admin" backButtonHref="" backButtonLabel="">
-        <RoleGate allowedRole={UserRole.ADMIN}>
-          <FormStatus status="success" message="You are allowed to see this content!" />
-        </RoleGate>
+        <>
+          <RoleGate allowedRole={UserRole.ADMIN}>
+            <FormStatus status="success" message="You are allowed to see this content!" />
+          </RoleGate>
+          <div className={styles.section}>
+            <p>Admin-only API route</p>
+            <Button variant="contained">Click to test</Button>
+          </div>
+          <div className={styles.section}>
+            <p>Admin-only Server Action</p>
+            <Button variant="contained">Click to test</Button>
+          </div>
+        </>
       </CardWrapper>
     </div>
   );
