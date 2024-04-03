a5f032308505e7a6ff58481f09306a20a16902c4
Bojana Armacki Dragutinovic <dragutinovic.bojana@gmail.com>
2024-04-03 13:53:53 +0200

    feat(settings): implement settings page;
    envoke settings server action with dummy data

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,11 +1,33 @@
 'use client';
 
-import { useCurrentUser } from '@/hooks/use-current-user';
+import { CardWrapper } from '@/components/auth/card-wrapper/card-wrapper';
+import styles from './page.module.css';
+import { Button } from '@/components/button';
+import { settings } from '@/actions/settings';
+import { useTransition } from 'react';
 
 const SettingsPage = () => {
-  const user = useCurrentUser();
+  const [isPending, startTransition] = useTransition();
 
-  return <div>{JSON.stringify(user)}</div>;
+  const onClick = () => {
+    startTransition(() => {
+      settings({
+        name: 'New Name',
+      });
+    });
+  };
+
+  return (
+    <div className={styles.container}>
+      <CardWrapper headerLabel="Settings" backButtonLabel="" backButtonHref="">
+        <>
+          <Button disabled={isPending} variant="contained" onClick={onClick}>
+            Update Name
+          </Button>
+        </>
+      </CardWrapper>
+    </div>
+  );
 };
 
 export default SettingsPage;
